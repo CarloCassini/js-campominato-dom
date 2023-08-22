e0c6670842c5a0b4b061702da72d5cc7ab8e1932
CarloCassini <c.cassini87@gmail.com>
2023-08-22 13:49:01 +0200

    start

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const cellsContainer = document.getElementById("contenitore-griglia");
+const myButton = document.getElementById("genera-griglia");
+
+myButton.addEventListener("click", function () {
+  const difficulty = document.getElementById("difficolta").value;
+  console.log(difficulty);
+  let cellsTotal;
+  if (difficulty == 1) {
+    cellsTotal = 10 * 10;
+  } else if (difficulty == 2) {
+    cellsTotal = 9 * 9;
+  } else {
+    cellsTotal = 7 * 7;
+  }
+
+  generaGriglia(cellsTotal, cellsContainer);
+});
